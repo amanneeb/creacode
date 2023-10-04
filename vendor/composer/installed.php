c737f0255ce9f5da64797ecb35ca90845b4e0f47
Marianne Anctil <2163768@csfoy.ca>
2023-10-04 09:23:55 -0400

    ajout blade + config MVC

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c5e1058bc35cbdc490594788430ee43f77e577b0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c5e1058bc35cbdc490594788430ee43f77e577b0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'eftec/bladeone' => array(
+            'pretty_version' => '4.1',
+            'version' => '4.1.0.0',
+            'reference' => 'd3e1efa1c6f776aa87fe47164d77e7ea67fc196f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../eftec/bladeone',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
