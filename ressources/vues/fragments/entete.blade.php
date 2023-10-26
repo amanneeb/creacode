da972f88e4a869998c1f2c65144d7e6921b16d4d
Marianne Anctil <2163768@csfoy.ca>
2023-10-26 15:42:48 -0400

    css menu mobile + js menu hamburger

--- ressources/vues/fragments/entete.blade.php
+++ ressources/vues/fragments/entete.blade.php
@@ -1,8 +1,21 @@
-<h3>Entête</h3>
-<ul>
+<img class="menu__logo" src="./liaisons/images/logo.svg" alt="" width="200px">
+<label for="recherche"></label>
+<input name="recherche" id="recherche" type="search" class="recherche" content="Rechercher un livre...">
+<button class="menu_btn compte">Mon compte</button>
+<button class="menu_btn panier">Panier</button>
+<nav id="navigation" class="nav navMobile navTable">
+    <div id="menuToggle">
+        <input type="checkbox" id="checkboxMenu" />
+
+        <span></span>
+        <span></span>
+        <span></span>
+<ul id="menu" class="menu_list">
     <li><a href="index.php?controleur=site&action=accueil">Accueil</a></li>
     <li><a href="index.php?controleur=livre&action=index">Livres</a></li>
     <li><a href="index.php?controleur=artiste&action=index">Artistes</a></li>
     <li><a href="">Galerie/Boutique</a></li>
     <li><a href="">Production la pastèque</a></li>
 </ul>
+    </div>
+</nav>
