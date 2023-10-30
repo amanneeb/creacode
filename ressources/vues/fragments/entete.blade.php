7d308084da9aca18ca41e9e1d8c9e8615c0b78e5
Marianne Anctil <2163768@csfoy.ca>
2023-10-30 14:10:25 -0400

    html footer fait + css menu et footer

--- ressources/vues/fragments/entete.blade.php
+++ ressources/vues/fragments/entete.blade.php
@@ -1,10 +1,14 @@
 
 <img class="menu__logo" src="./liaisons/images/logo.svg" alt="" width="200px">
 {{--<h1>La pastèque</h1>--}}
-<label for="recherche"></label>
+<div class="barRecherche">
+<label for="recherche">Rechercher un livre</label>
 <input name="recherche" id="recherche" type="search" class="recherche" content="Rechercher un livre...">
+</div>
+<div class="buttons">
 <button class="menu_btn compte">Mon compte</button>
 <button class="menu_btn panier">Panier</button>
+</div>
 <nav id="navigation" class="nav navMobile navTable">
     <div id="menuToggle">
         <input type="checkbox" id="checkboxMenu" />
