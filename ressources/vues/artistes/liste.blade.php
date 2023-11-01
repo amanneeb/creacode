59e575ce85261ae4832f690b033b2508c17de535
Giselle Hessen <1841651@csfoy.ca>
2023-11-01 09:16:33 -0400

    modification des liens vers la fiche des artistes

--- ressources/vues/artistes/liste.blade.php
+++ ressources/vues/artistes/liste.blade.php
@@ -6,7 +6,7 @@
     <ul class="liste-auteurs__items">
         @foreach ($auteurs as $auteur)
             <li class="auteur">
-                <a href="index.php?controleur=artistes&action=fiche&idArtiste={{ $auteur->getId() }}">
+                <a href="index.php?controleur=artiste&action=fiche&idArtiste={{ $auteur->getId() }}">
                     <img src="auteur{{ $auteur->getId() }}.jpg" alt="{{ $auteur->getPrenomNom() }}" class="auteur__image">
                     <h3 class="auteur__nom">{{ $auteur->getPrenomNom() }}</h3>
                 </a>
