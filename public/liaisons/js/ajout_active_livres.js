f7ad3a901eab78f1ea086786e0d9c03b6efc98b8
Giselle Hessen <1841651@csfoy.ca>
2023-11-06 12:42:18 -0500

    bouton switch grille liste

--- public/liaisons/js/ajout_active_livres.js
+++ public/liaisons/js/ajout_active_livres.js
@@ -1,10 +1,26 @@
-const options = document.querySelectorAll('.liste-livres__tri-option');
+document.addEventListener("DOMContentLoaded", function () {
+    let boutonListe = document.getElementById("bouton-liste");
+    let boutonGrille = document.getElementById("bouton-grille");
+    let imageItems = document.querySelectorAll(".item-img");
+    let listeItems = document.querySelector(".liste__items");
 
-options.forEach(function(option) {
-    option.addEventListener('click', function() {
-        options.forEach(function(opt) {
-            opt.classList.remove('active');
+    boutonGrille.addEventListener("click", function () {
+        boutonGrille.classList.add("actif");
+        boutonListe.classList.remove("actif");
+        listeItems.classList.remove("liste__items");
+
+        imageItems.forEach(function (imageItem) {
+            imageItem.hidden = true;
         });
-        this.classList.add('active');
+    });
+
+    boutonListe.addEventListener("click", function () {
+        boutonListe.classList.add("actif");
+        boutonGrille.classList.remove("actif");
+        listeItems.classList.add("liste__items");
+
+        imageItems.forEach(function (imageItem) {
+            imageItem.hidden = false;
+        });
     });
 });
